fa38d558644d6b907d0cae1639f1b4034c3fd198
seongukHEO <heoseonguk3@gmail.com>
2024-08-29 04:02:44 +0900

    feat : dto 구현

--- lib/data/data_source/mock/display/display_mock_api.dart
+++ lib/data/data_source/mock/display/display_mock_api.dart
@@ -2,20 +2,21 @@ import 'dart:convert';
 
 import '../../../dto/display/menu/menu.dto.dart';
 import '../../remote/display.api.dart';
+import 'display_mock_data.dart';
 
-class DisplayMockApi implements DisplayApi{
+class DisplayMockApi implements DisplayApi {
   @override
   Future<List<MenuDto>> getMenusByMallType(String mallType) {
-    // TODO: implement getMenusByMallType
-    throw UnimplementedError();
+    return Future(() => _menuParser(mallType == 'market'
+        ? DisplayMockData.menusByMarket
+        : DisplayMockData.menusByBeauty));
   }
 
   //String을 MenuDto로 바꿔주는 Parser
-  List<MenuDto> _menuParser(String source){
+  List<MenuDto> _menuParser(String source) {
     List<MenuDto> menus = [];
     final List json = jsonDecode(source);
     menus = json.map((e) => MenuDto.fromJson(e)).toList();
     return menus;
   }
-
 }
