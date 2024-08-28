be7b63e8001d6979ac520bbcc3ab88f997711833
seongukHEO <heoseonguk3@gmail.com>
2024-08-29 04:50:16 +0900

    feat : response_wrapper.dart 구현

--- lib/data/data_source/mock/display/display_mock_api.dart
+++ lib/data/data_source/mock/display/display_mock_api.dart
@@ -1,15 +1,21 @@
 import 'dart:convert';
 
+import '../../../dto/common/response_wrapper/response_wrapper.dart';
 import '../../../dto/display/menu/menu.dto.dart';
 import '../../remote/display.api.dart';
 import 'display_mock_data.dart';
 
 class DisplayMockApi implements DisplayApi {
   @override
-  Future<List<MenuDto>> getMenusByMallType(String mallType) {
-    return Future(() => _menuParser(mallType == 'market'
-        ? DisplayMockData.menusByMarket
-        : DisplayMockData.menusByBeauty));
+  Future<ResponseWrapper<List<MenuDto>>> getMenusByMallType(String mallType) {
+    return Future(() => ResponseWrapper(
+      status: 'Success',
+      code: '0000',
+      message: "",
+      data: _menuParser(mallType == 'market'
+          ? DisplayMockData.menusByMarket
+          : DisplayMockData.menusByBeauty)
+    ));
   }
 
   //String을 MenuDto로 바꿔주는 Parser
--- lib/data/data_source/remote/display.api.dart
+++ lib/data/data_source/remote/display.api.dart
@@ -5,6 +5,7 @@ import 'package:json_annotation/json_annotation.dart';
 import 'package:retrofit/http.dart';
 import 'package:retrofit/retrofit.dart';
 
+import '../../dto/common/response_wrapper/response_wrapper.dart';
 import '../../dto/display/menu/menu.dto.dart';
 
 part 'display.api.g.dart';
@@ -14,5 +15,5 @@ abstract class DisplayApi {
   factory DisplayApi(Dio dio, {String baseUrl}) = _DisplayApi;
 
   @GET('/api/menus/{mall_type}')
-  Future<List<MenuDto>> getMenusByMallType(@Path('mall_type') String mallType);
+  Future<ResponseWrapper<List<MenuDto>>> getMenusByMallType(@Path('mall_type') String mallType);
 }
